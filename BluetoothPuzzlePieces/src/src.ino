a16d1107d057fe595094e54a6dab5e45f642c499
MichaelRol <michael.rollins@hotmail.co.uk>
2019-11-24 01:00:20 +0000

    Got board talking to Laptop, but not doing anything productive

---
+++ BluetoothPuzzlePieces/src/src.ino
@@ -0,0 +1,11 @@
+void setup() {
+
+    pinMode(12, OUTPUT); //LED INPUT
+    pinMode(11, INPUT); //BUTTON TOUCHED
+    digitalWrite(12, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+} 
